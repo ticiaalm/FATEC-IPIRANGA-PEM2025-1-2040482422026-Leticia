0e95ce5f63af7b271eff023934b77dceaa1b46e6
Letícia Almeida Silva <leticiaalmeidadev@gmail.com>
2025-04-04 11:10:03 -0300

    Add files via upload

---
+++ PEM2025-1-Atividades-N1-N2/PEM2025-1-Atividades-N1/PEM-Atividade-N1-4/PEM-Atividade-N1-4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void listar_diretorios (const char *caminho, int nivel) {
+    struct dirent *entrada;
+    DIR *dir = opendir(caminho);
+    if (dir == NULL) {
+        perror("Erro ao abrir diretorio");
+        return;
+    }
+    while ((entrada = readdir(dir)) != NULL) {
+        if (strcmp (entrada->d_name, ".") == 0 || strcmp (entrada->d_name, "..") == 0) {
+            continue;
+        }
+        for (int i = 0; i < nivel; i++) {
+            printf("    ");
+        }
+        printf("-> %s\n", entrada->d_name);
+        char caminho_completo[1024];
+        snprintf (caminho_completo, sizeof(caminho_completo), "%s/%s", caminho, entrada->d_name);
+        struct stat info;
+        if (stat(caminho_completo, &info) == 0 && S_ISDIR(info.st_mode)) {
+            listar_diretorios(caminho_completo, nivel + 1);
+        }
+    }
+    closedir (dir);
+}
+int main () {
+    const char *caminho_inicial = ".";
+    printf ("Estrutura do diretorio:\n");
+    listar_diretorios(caminho_inicial, 0);
+    return 0;
+}
